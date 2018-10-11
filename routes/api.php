68baa263e281b5a0da0fc278235a8af6c4ed92bb
Smirnoff Igor <smirnoffnew@gmail.com>
2018-10-11 18:42:28 +0300

    minimal api work

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,20 @@ use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+|--------------------------------------------------------------------------|
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::post('signin', 'AuthController@login');
+Route::post('signup', 'AuthController@signup');
+
+Route::group([
+    'middleware' => 'auth:api'
+], function() {
+    Route::get('logout', 'AuthController@logout');
+    Route::get('user', 'AuthController@user');
 });
